729a2843b88986f6091fbe89cc145d1c76119ae7
eenblam <ben.a.elam@gmail.com>
2018-04-25 12:10:53 -0700

    Basic assets and bot.py
    bot.py from https://github.com/Rapptz/discord.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import os
+
+import discord
+import asyncio
+
+from secrets import token
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    s = 'Logged in as {} with {}'.format(client.user.name, client.user.id)
+    print(s)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='pnbot',
+        version='0.0.1',
+        packages=find_packages(),
+        install_requires=[
+            'discord.py',
+            ],
+        entry_points={},
+        )
