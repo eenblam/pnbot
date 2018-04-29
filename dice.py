3087325bc83c761e6bf6636e33c41499326307d5
Chase <chasebaker288@gmail.com>
2018-04-29 02:10:23 -0500

    Update dice.py

--- dice.py
+++ dice.py
@@ -3,7 +3,7 @@ from random import randint
 
 def roll(dice_count, dice_size=0, explode_criteria=0):
 	if dice_size == 0:
-		return dice_count
+		return [dice_count]
 	else:
 		if explode_criteria >= dice_size:
 			explode_criteria = 0
@@ -11,42 +11,52 @@ def roll(dice_count, dice_size=0, explode_criteria=0):
 		for i in range(dice_count):
 			die = randint(1, dice_size)
 			rolls.append(die)
-			while die > (dice_size - explode_criteria):
+			while die > (dice_size-explode_criteria):
 				die = randint(1, dice_size)
 				rolls.append(die)
 		return rolls
 
 
-def drop_low(rolls, count=1)
-	if count >= len(rolls):
+def drop_low(rolls, number=1):
+	if number >= len(rolls):
 		return []
 	else:
 		rolls.sort()
-		return rolls[count:]
+		return rolls[number:]
 
 
-def drop_high(rolls, count=1)
-	if count >= len(rolls):
+def drop_high(rolls, number=1):
+	if number >= len(rolls):
 		return []
 	else:
 		rolls.sort()
-		return rolls[:-count]
+		return rolls[:-number]
 
 
-def count(rolls, hit)
+def count(rolls, hit):
 	return len([i for i in rolls if i >= hit])
 
 
-def parse(string)  # WIP
-	unparsed = string.split("d")
-	EXPLOSIONS = len([x for x in unparsed[1] if x == "!"])
-	# unparsed[1] = str
-	roll(INPUT[0], INPUT[1], EXPLOSIONS)
+def parse(string):
+	UNPARSED = string.split("d")
+	EXPLOSIONS = len([x for x in UNPARSED[1] if x == "!"])
+	roll(int(UNPARSED[0]), int(UNPARSED[1][:-EXPLOSIONS]), EXPLOSIONS)
 
 
-def preparse(string)  #WIP
-	to_sum = string.split(" + ")
-	asdf = [parse(i) for i in to_sum]
+def preparse(string):
+	check = string.split(" ")
+	sums = []
+	rolls = []
+	negative = False
+	for i in range(len(check)):
+		if check[i] == "-":
+			negative = True
+		elif check[i] not in ["-", "+"]:
+			rolls.append(parse(check[i]))
+			sums.append(sum(rolls[-1])*((-1)**negative))
+			negative = False
+	return [sum(sums), rolls]
+
 	
 
 
