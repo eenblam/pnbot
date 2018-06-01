5eb3083c3442a5f7e164ca9130b44d21e18854fa
Chase <chasebaker288@gmail.com>
2018-06-01 15:19:13 -0500

    Update auction.py

--- auction/auction.py
+++ auction/auction.py
@@ -1,20 +1,40 @@
-# I lost track of what I was doing partway through writing this, so I'm leaving it here for now
+def find_winner(bids):
+    if len(bids) == 0:
+        successful = False
+        reason = "No bids were placed."
+    else:
+        best_bid = max([bids[x] for x in bids])
+        winners = [x for x in bids if bids[x] == best_bid]
+        if len(winners) == 1:
+            successful = True
+            reason = "The winner is **" + str(winners[0])[:-5] + "**, with a bid of " + str(best_bid) + "!"
+        else:
+            successful = False
+            reason = "There was a tie.\n**" + str(winners[0])[:-5]
+            for i in winners[1:]:
+                reason += "**, **" + str(i)[:-5]
+            reason += "** each bet *" + str(best_bid) + "*!"
+    if successful:
+        return [reason, winners[0], best_bid]
+    else:
+        return ["Auction failed: " + reason, 0]
 
-class Auction:
-    def __init__(self, item_list):
-        self.items = item_list
-        self.members = {}
-        self.wagers = {}
-        for i in item_list:
-            self.wagers[i] = {}
-    def add_member(self, user):
-        self.members[str(user), 100]
-    def collect_wager(self, user, proposed_wager):
-        wager = int(proposed_wager)
-        if self.members[str(user)] < wager:
-            wager = self.members[str(user)]
 
-# Seriously though, I can't remember how this was supposed to work under the hood.
-
-
-
+def redist(wagers, extra):
+    wagers = [x for x in wagers if x[1] != 0]
+    if len(wagers) == 1:
+        return [[], extra + wagers[0][1]]
+    else:
+        best = max([x[1] for x in wagers])
+        to_distribute = extra + best
+        total = sum([x[1] for x in wagers]) - best
+        remnants = 0
+        for x in wagers:
+            if x[1] == best:
+                x[1] = 0
+            else:
+                x[1] *= to_distribute
+                x[1] /= total
+                remnants += x[1] - int(x[1])
+                x[1] = int(x[1])
+        return [wagers, int(remnants)]
