9f3586499376150a66321f93ab95eb02a1276773
Chase <chasebaker288@gmail.com>
2018-07-03 13:49:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,23 +43,16 @@ async def on_message(message):
         await client.send_message(message.channel, 'Done sleeping')
     elif message.content.startswith("!roll"):  # Example: "!roll 3d6".
         await client.send_message(message.channel, prepreparse(message.content[6:]))
-    elif message.content.startswith("!map") and not BUSY:  # Example: "!map 400, 800".
-        BUSY = True
-        dimensions = message.content[5:].split(", ")
-        await client.send_message(message.channel, "Building map, please wait...")
-        save_to_image(
-            delake(
-                blur(
-                    walk(
-                        build(
-                            map_parse(dimensions)
-                        )
-                    )
-                )
-            ), LOCAL_STORAGE
-        )
-        await client.send_file(message.channel, LOCAL_STORAGE)
-        BUSY = False
+    elif message.content.startswith("!map"):  # Example: "!map 400, 800".
+        if BUSY:
+            await client.send_message(message.channel, "I am currently busy. Please try again in a moment.")
+        else:
+            BUSY = True
+            dimensions = message.content[5:].split(", ")
+            await client.send_message(message.channel, "Building map, please wait...")
+            generate(dimensions, LOCAL_STORAGE)
+            await client.send_file(message.channel, LOCAL_STORAGE)
+            BUSY = False
     elif message.content.startswith("!secret"):
         await client.send_message(message.author, sneakret())
     elif message.content.startswith("!eval"):
@@ -74,41 +67,42 @@ async def on_message(message):
             await client.send_message(message.author, "No wager found.\n*Your current balance is " + str(GOLD[str(message.author)]) + " Gold.*")
         else:
             await client.send_message(message.author, "Your current wager is " + str(MESSAGE_LOGS[str(message.author)]) + " out of " + str(GOLD[str(message.author)]) + " Gold.")
-    elif message.content.startswith("!auction") and message.channel not in client.private_channels and not BUSY:
-        BUSY = True
-        for i in MESSAGE_LOGS:
-            MESSAGE_LOGS[i] = 0
-        await client.send_message(message.channel, "**BEGINNING SECRET AUCTION!**\nPlease send me a direct message with your wager.")
-        await asyncio.sleep(30)
-        await client.send_message(message.channel, "30 seconds remaining.")
-        await asyncio.sleep(20)
-        await client.send_message(message.channel, "Auction closing in 10 seconds...")
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, "5")
-        await asyncio.sleep(1)
-        await client.send_message(message.channel, "4")
-        await asyncio.sleep(1)
-        await client.send_message(message.channel, "3")
-        await asyncio.sleep(1)
-        await client.send_message(message.channel, "2")
-        await asyncio.sleep(1)
-        await client.send_message(message.channel, "1")
-        await asyncio.sleep(1)
-        result = find_winner(MESSAGE_LOGS)
-        todo = redist(MESSAGE_LOGS, GOLD["pnbot#0050"])
-        if len(result) != 1:
-            GOLD[result[1]] -= result[2]
-            GOLD["pnbot#0050"] = todo[1]
-            for x in todo[0]:
-                GOLD[x[0]] += x[1]
+    elif message.content.startswith("!auction") and message.channel not in client.private_channels:
+        if BUSY:
+            await client.send_message(message.channel, "I am currently busy. Please try again in a moment.")
         else:
-            GOLD["pnbot#0050"] += result[2]
-        for i in MESSAGE_LOGS.keys():
-            MESSAGE_LOGS[i] = 0
-        await client.send_message(message.channel, result[0])
-        BUSY = False
-    elif (message.content.startswith("!auction") or message.content.startswith("!map")) and BUSY:
-        await client.send_message(message.author, "I am currently busy. Please try again in a moment.")
+            BUSY = True
+            for i in MESSAGE_LOGS:
+                MESSAGE_LOGS[i] = 0
+            await client.send_message(message.channel, "**BEGINNING SECRET AUCTION!**\nPlease send me a direct message with your wager.")
+            await asyncio.sleep(30)
+            await client.send_message(message.channel, "30 seconds remaining.")
+            await asyncio.sleep(20)
+            await client.send_message(message.channel, "Auction closing in 10 seconds...")
+            await asyncio.sleep(5)
+            await client.send_message(message.channel, "5")
+            await asyncio.sleep(1)
+            await client.send_message(message.channel, "4")
+            await asyncio.sleep(1)
+            await client.send_message(message.channel, "3")
+            await asyncio.sleep(1)
+            await client.send_message(message.channel, "2")
+            await asyncio.sleep(1)
+            await client.send_message(message.channel, "1")
+            await asyncio.sleep(1)
+            result = find_winner(MESSAGE_LOGS)
+            todo = redist(MESSAGE_LOGS, GOLD["pnbot#0050"])
+            if len(result) != 1:
+                GOLD[result[1]] -= result[2]
+                GOLD["pnbot#0050"] = todo[1]
+                for x in todo[0]:
+                    GOLD[x[0]] += x[1]
+            else:
+                GOLD["pnbot#0050"] += result[2]
+            for i in MESSAGE_LOGS.keys():
+                MESSAGE_LOGS[i] = 0
+            await client.send_message(message.channel, result[0])
+            BUSY = False
     elif str(message.author) != "pnbot#0050" and message.channel in client.private_channels:
         try:
             MESSAGE_LOGS[str(message.author)] = min(GOLD[str(message.author)], max(0, int(message.content)))
