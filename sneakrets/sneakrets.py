34c792e2b4d917a5aae5f9e0f161f8e930bb06e3
Chase <chasebaker288@gmail.com>
2018-07-03 13:41:37 -0500

    Update sneakrets.py

--- sneakrets/sneakrets.py
+++ sneakrets/sneakrets.py
@@ -4,13 +4,13 @@ LIST_OF_SECRETS = [
     "An octopus is just a boneless spider.",
     "10 = 10, but not in base 1.",
     "A hippopotamus is just a really cool Opotamus.",
-    "You can toggle the [Jog] ability by pressing the [Walk] key while already walking.",
+    "You can toggle the [Jog] ability by double tapping the [Walk] key.",
     "The human colon actually has a second sphincter muscle.",
     "Taxation is theft.",
     "If left long enough to ferment, grapes of wrath become a bitter whine.",
-    "The term 'childbirth' is either redundant or has frightening implications.",
+    "The term 'childbirth' is either redundant, or has concerning implications.",
     "Despite their name, dragons rarely drag things onto other things.",
-    "Babies are neither commonly found in bays, nor are they a type of bee.",
+    "Babies are not actually a type of bee, nor are they native the bay area.",
     "You can ask me to !eval python 3 expressions"
 ]
 
