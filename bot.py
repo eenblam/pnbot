9f25eb1f6223b0ae224777cbaa36e19a658022a4
eenblam <ben.a.elam@gmail.com>
2018-07-13 12:26:05 -0700

    Add auction error handling
    Also *always* sets BUSY=false, even if error occurs during auction

--- bot.py
+++ bot.py
@@ -72,42 +72,65 @@ async def on_message(message):
             await client.send_message(message.channel, "I am currently busy. Please try again in a moment.")
         else:
             BUSY = True
-            for i in MESSAGE_LOGS:
-                MESSAGE_LOGS[i] = 0
-            await client.send_message(message.channel, "**BEGINNING SECRET AUCTION!**\nPlease send me a direct message with your wager.")
-            await asyncio.sleep(30)
-            await client.send_message(message.channel, "30 seconds remaining.")
-            await asyncio.sleep(20)
-            await client.send_message(message.channel, "Auction closing in 10 seconds...")
-            await asyncio.sleep(5)
-            await client.send_message(message.channel, "5")
-            await asyncio.sleep(1)
-            await client.send_message(message.channel, "4")
-            await asyncio.sleep(1)
-            await client.send_message(message.channel, "3")
-            await asyncio.sleep(1)
-            await client.send_message(message.channel, "2")
-            await asyncio.sleep(1)
-            await client.send_message(message.channel, "1")
-            await asyncio.sleep(1)
-            result = find_winner(MESSAGE_LOGS)
-            todo = redist(MESSAGE_LOGS, GOLD["pnbot#0050"])
-            if len(result) != 1:
-                GOLD[result[1]] -= result[2]
-                GOLD["pnbot#0050"] = todo[1]
-                for x in todo[0]:
-                    GOLD[x[0]] += x[1]
-            else:
-                GOLD["pnbot#0050"] += result[2]
-            for i in MESSAGE_LOGS.keys():
-                MESSAGE_LOGS[i] = 0
-            await client.send_message(message.channel, result[0])
-            BUSY = False
+            try:
+                for i in MESSAGE_LOGS:
+                    MESSAGE_LOGS[i] = 0
+
+                await client.send_message(message.channel, "**BEGINNING SECRET AUCTION!**\nPlease send me a direct message with your wager.")
+                await asyncio.sleep(30)
+                await client.send_message(message.channel, "30 seconds remaining.")
+                await asyncio.sleep(20)
+                await client.send_message(message.channel, "Auction closing in 10 seconds...")
+                await asyncio.sleep(5)
+                await client.send_message(message.channel, "5")
+                await asyncio.sleep(1)
+                await client.send_message(message.channel, "4")
+                await asyncio.sleep(1)
+                await client.send_message(message.channel, "3")
+                await asyncio.sleep(1)
+                await client.send_message(message.channel, "2")
+                await asyncio.sleep(1)
+                await client.send_message(message.channel, "1")
+                await asyncio.sleep(1)
+                result = find_winner(MESSAGE_LOGS)
+                todo = redist(MESSAGE_LOGS, GOLD["pnbot#0050"])
+                if len(result) != 1:
+                    GOLD[result[1]] -= result[2]
+                    GOLD["pnbot#0050"] = todo[1]
+                    for x in todo[0]:
+                        GOLD[x[0]] += x[1]
+                else:
+                    GOLD["pnbot#0050"] += result[2]
+                for i in MESSAGE_LOGS.keys():
+                    MESSAGE_LOGS[i] = 0
+                await client.send_message(message.channel, result[0])
+            finally:
+                BUSY = False
     elif str(message.author) != "pnbot#0050" and message.channel in client.private_channels:
         try:
-            MESSAGE_LOGS[str(message.author)] = min(GOLD[str(message.author)], max(0, int(message.content)))
-        except:
-            pass
+            author = str(message.author)
+            balance = GOLD[author]
+            requested_wager = int(message.content)
+        except KeyError:
+            await client.send_message(message.author, "Sorry, I couldn't find your balance.")
+            return
+        except ValueError:
+            # Couldn't coerce content to int
+            await client.send_message(message.author, "Sorry, I didn't undestand that. To participate in an auction, send a nonnegative integer as your wager.")
+            return
+
+        if requested_wager < 0:
+            # Negative wagers are ignored.
+            await client.send_message(message.author, "Sorry, I can't do much with a negative wager.")
+            return
+        if requested_wager > balance:
+            MESSAGE_LOGS[author] = balance
+            await client.send_message(message.author, "Sorry, you don't have enough for that. Using your available balance ({}). To cancel, send a \"0\"".format(balance))
+            return
+
+        #TODO kinda hairy having "MESSAGE_LOGS" that log something other than what was said
+        MESSAGE_LOGS[author] = requested_wager
+        await client.send_message(message.author, "Accepted wager of {}".format(requested_wager))
 
 
 client.run(token)
